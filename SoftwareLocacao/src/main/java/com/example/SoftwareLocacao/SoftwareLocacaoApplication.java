616af94a9ba4d859ffc1e8cd8aa2f3224bd8cc98
Ana Beatriz <caldas.ana9@gmail.com>
2023-10-23 15:22:17 -0300

    build: inicialização do projeto

---
+++ SoftwareLocacao/src/main/java/com/example/SoftwareLocacao/SoftwareLocacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoftwareLocacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareLocacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareLocacaoApplication.class, args);
+	}
+
+}
---
+++ SoftwareLocacao/src/test/java/com/example/SoftwareLocacao/SoftwareLocacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoftwareLocacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareLocacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
