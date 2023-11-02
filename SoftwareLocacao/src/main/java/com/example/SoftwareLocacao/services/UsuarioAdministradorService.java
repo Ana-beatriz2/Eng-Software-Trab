6ff611c3e9d337cee9fcfc70e2904b85bad335ca
Ana Beatriz <caldas.ana9@gmail.com>
2023-11-02 20:43:27 -0300

    fix(UsuarioAdmService): ajuste no findById

--- SoftwareLocacao/src/main/java/com/example/SoftwareLocacao/services/UsuarioAdministradorService.java
+++ SoftwareLocacao/src/main/java/com/example/SoftwareLocacao/services/UsuarioAdministradorService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 public class UsuarioAdministradorService {
 
@@ -15,7 +17,7 @@ public class UsuarioAdministradorService {
     public UsuarioAdministrador findById(Long id){
         Optional<UsuarioAdministrador> usuarioAdministrador = this.usuarioAdmRepository.findById(id);
         return usuarioAdministrador.orElseThrow( () -> new RuntimeException(
-           "Usuário não encontrado! id: " + id + ", Tipo: " + UsuarioAdministrador.class.getNome()
+           "Usuário não encontrado! id: " + id + ", Tipo: " + UsuarioAdministrador.class.getName()
         ));
     }
 
