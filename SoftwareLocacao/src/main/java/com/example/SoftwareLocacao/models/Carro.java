2024ee3882c2c38010344cb6986ccbb88c58d090
SarahElesbao <79275710+SarahElesbao@users.noreply.github.com>
2023-11-14 20:06:53 -0300

    model carro

--- SoftwareLocacao/src/main/java/com/example/SoftwareLocacao/models/Carro.java
+++ SoftwareLocacao/src/main/java/com/example/SoftwareLocacao/models/Carro.java
@@ -1,4 +1,35 @@
 package com.example.SoftwareLocacao.models;
 
-public class Carro {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = Carro.TABLE_NAME)
+public class Carro{
+    public static final String TABLE_NAME = "carro";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "marca", nullable = true, unique = true)
+    private String marca;
+
+    @Column(name = "ano", nullable = false)
+    private int ano;
+
+    @Column(name = "placa", nullable = true, unique = true)
+    private String placa;
+
+    @Column(name = "cor", nullable = true)
+    private String cor;
+
+    @Column(name = "tipoCombustivel", nullable = true)
+    private String tipoCombustivel;
+
+    @Column(name = "statusCarro", nullable = true)
+    private String statusCarro;
+
+    @Column(name = "quilometragem", nullable = true, unique = true)
+    private int quilometragem;
 }
