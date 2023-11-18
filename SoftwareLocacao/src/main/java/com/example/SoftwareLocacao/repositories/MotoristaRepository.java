0a10013738d10c7c9b93568b2b91e68851a0a6e8
Ana Beatriz <caldas.ana9@gmail.com>
2023-11-18 16:49:43 -0300

    feat: crud do motorista finalizado

---
+++ SoftwareLocacao/src/main/java/com/example/SoftwareLocacao/repositories/MotoristaRepository.java
@@ -0,0 +1,8 @@
+package com.example.SoftwareLocacao.repositories;
+
+import com.example.SoftwareLocacao.models.Motorista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
+
+}
---
+++ SoftwareLocacao/src/main/java/com/example/SoftwareLocacao/services/MotoristaService.java
@@ -0,0 +1,51 @@
+package com.example.SoftwareLocacao.services;
+
+import com.example.SoftwareLocacao.models.Motorista;
+import com.example.SoftwareLocacao.models.UsuarioAdministrador;
+import com.example.SoftwareLocacao.repositories.MotoristaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class MotoristaService {
+
+    @Autowired
+    private MotoristaRepository motoristaRepository;
+
+    public Motorista findMotoristaById(Long id){
+        Optional<Motorista> motorista = this.motoristaRepository.findById(id);
+        return motorista.orElseThrow( () -> new RuntimeException(
+                "Motorista não encontrado! id: " + id + ", Tipo: " + Motorista.class.getName()
+        ));
+    }
+
+    @Transactional
+    public Motorista createMotorista(Motorista obj){
+        obj.setId(null);
+        obj = this.motoristaRepository.save(obj);
+        return obj;
+    }
+    @Transactional
+    public Motorista updateMotorista(Motorista obj) {
+        Motorista newObj = findMotoristaById(obj.getId());
+        newObj.setNome(obj.getNome());
+        newObj.setCnh(obj.getCnh());
+        newObj.setDtNascimento(obj.getDtNascimento());
+        newObj.setCpf(obj.getCpf());
+        newObj.setEmail(obj.getEmail());
+        newObj.setAds(obj.getAds());
+        return this.motoristaRepository.save(newObj);
+    }
+
+    public void deleteMotorista(Long id){
+        findMotoristaById(id);
+        try {
+            this.motoristaRepository.deleteById(id);
+        } catch (Exception e){
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
+}
